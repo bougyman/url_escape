675acda66e04c8f8fdb29a226b6b0042fcd1c90b
TJ Vanderpoel <Jayson Vaughn jv@rubyists.com>
2009-06-22 13:21:36 -0500

    Cleaned up variable names Removed high nibble calculations

--- ext/escape.c
+++ ext/escape.c
@@ -13,11 +13,11 @@ THE SOFTWARE IS PROVIDED ‘AS IS’, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR I
 
 #include "ruby.h"
 
-#ifndef RSTRING_PTR(obj)
+#ifndef RSTRING_PTR
 #define RSTRING_PTR(obj) RSTRING(obj)->ptr
 #endif
 
-#ifndef RSTRING_LEN(obj)
+#ifndef RSTRING_LEN
 #define RSTRING_LEN(obj) RSTRING(obj)->len
 #endif
 
@@ -122,24 +122,20 @@ static VALUE escape(VALUE self, VALUE str)
   VALUE outstr = rb_str_buf_new(len);
 
   int i;
-  char mx, nx;
-  char m;
+  unsigned char byte_two, byte_three;
 
   for(i = 0; i < len;) {
-    const char byte = buf[i];
-
-    // high 4 bits from byte, eg L from 0xLX
-    const char high_bits = (byte >> 4) & 0xf;
+    const unsigned char byte_one = buf[i];
 
     /* (UTF-8 escape, 0x0000-0x007F) */
-    if(high_bits < 0x8) {
-      if(valid_literal(byte)) {
-        rb_str_buf_cat(outstr, &byte, 1);
-      } else if(byte == ' ') {
+    if(byte_one < 0x80) {
+      if(valid_literal(byte_one)) {
+        rb_str_buf_cat(outstr, buf+i, 1);
+      } else if(byte_one == ' ') {
         // a + or %20 replacement (depending on const plus assignment)
         rb_str_buf_cat(outstr, plus, 1);
       } else { // It's ascii but needs encoding
-        rb_str_buf_cat(outstr, hex_table[byte & 0xff], 3);
+        rb_str_buf_cat(outstr, hex_table[byte_one], 3);
       }
       i++;
       continue;
@@ -150,25 +146,24 @@ static VALUE escape(VALUE self, VALUE str)
     //
     // If we have at least one extra byte to consume
     if(i + 1 < len) {
-      mx = buf[i + 1];
-      m = (mx >> 4) & 0xf;
+      byte_two = buf[i + 1];
 
       /* (UTF-8 escape, 0x0080-0x07FF) */
-      if(0xc <= high_bits && high_bits <= 0xd && 0x8 <= m && m <= 0xb) {
-        rb_str_buf_cat(outstr, hex_table[byte & 0xff], 3);
-        rb_str_buf_cat(outstr, hex_table[mx & 0xff], 3);
+      if(0xc0 <= byte_one && byte_one <= 0xdf && 0x80 <= byte_two && byte_two <= 0xbf) {
+        rb_str_buf_cat(outstr, hex_table[byte_one], 3);
+        rb_str_buf_cat(outstr, hex_table[byte_two], 3);
         i += 2;
         continue;
 
         // If we have at least two extra bytes to consume
       } else if(i + 2 < len) {
-        nx = buf[i + 2];
+        byte_three = buf[i + 2];
 
         /* (UTF-8 escape, 0x0800-0xFFFF) */
-        if(0xe == high_bits && 0x8 <= m && m <= 0xb) {
-          rb_str_buf_cat(outstr, hex_table[byte & 0xff], 3);
-          rb_str_buf_cat(outstr, hex_table[mx & 0xff], 3);
-          rb_str_buf_cat(outstr, hex_table[nx & 0xff], 3);
+        if(0xe0 == byte_one && 0x80 <= byte_two && byte_two <= 0xbf) {
+          rb_str_buf_cat(outstr, hex_table[byte_one], 3);
+          rb_str_buf_cat(outstr, hex_table[byte_two], 3);
+          rb_str_buf_cat(outstr, hex_table[byte_three], 3);
           i += 3;
           continue;
         }
@@ -176,11 +171,11 @@ static VALUE escape(VALUE self, VALUE str)
     }
 
     /* (ISO Latin-1/2/? escape, 0x0080 - x00FF) */
-    if(0x8 <= high_bits && high_bits <= 0xf) {
-      rb_str_buf_cat(outstr, hex_table[byte & 0xff], 3);
+    if(0x80 <= byte_one) {
+      rb_str_buf_cat(outstr, hex_table[byte_one], 3);
     } else {
       // Well crap. Just throw it in I guess...
-      rb_str_buf_cat(outstr, hex_table[byte & 0xff], 3);
+      rb_str_buf_cat(outstr, hex_table[byte_one], 3);
     }
     i++;
   }
