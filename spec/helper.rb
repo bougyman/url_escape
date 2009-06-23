472af853e1fa9b43d1b13c6f1b2d2822cfe49a81
Michael Fellinger <m.fellinger@gmail.com>
2009-06-24 01:18:18 +0900

    Use block chdir

--- spec/helper.rb
+++ spec/helper.rb
@@ -4,9 +4,13 @@ require "bacon"
 Bacon.summary_at_exit
 
 
-Dir.chdir ext = File.expand_path("../../ext", __FILE__)
-puts %x{make clean}
-File.unlink("Makefile") if File.file?("Makefile")
-puts %x{ruby extconf.rb}
-puts %x{make}
-require File.join(ext, "url_escape.so")
+ext = File.expand_path("../../ext", __FILE__)
+
+Dir.chdir ext do
+  puts %x{make clean}
+  File.unlink("Makefile") if File.file?("Makefile")
+  puts %x{ruby extconf.rb}
+  puts %x{make}
+end
+
+require File.join(ext, "url_escape")
