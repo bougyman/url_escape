cbb73f74c9d69e2cbd3d8fd0ad2b2a4cb33f917c
TJ Vanderpoel <TJ Vanderpoel tj@rubyists.com>
2009-06-23 09:03:46 -0500

    cleaned up bench spec

--- spec/bench.rb
+++ spec/bench.rb
@@ -4,23 +4,38 @@ require 'rack'
 require 'cgi'
 
 describe "URLEscape benchmark" do
-  def benchit(obj)
+  def benchit(obj, meth)
     bm = Benchmark.bmbm { |b|
       n = 100
-      s = "%%%%" * 10000
-      b.report("#{obj.name}::escape"){ n.times{ obj.escape(s) }}
+      s = "%03a" * 10000
+      b.report("#{obj.name}::#{meth}") { n.times{ obj.send(meth, s) } }
     }
   end
 
+  it "runs faster than standard CGI.unescape" do
+    one = benchit(URLEscape, :unescape).first
+    two = benchit(CGI, :unescape).first
+    p one.real
+    p two.real
+    one.real.should < 20.0 * two.real
+  end
+
+  it "runs faster than Rack::Utils.unescape" do
+    one = benchit(URLEscape, :unescape).first
+    two = benchit(Rack::Utils, :unescape).first
+    one.real.should < 20.0 * two.real
+  end
+
   it "runs faster than standard CGI.escape" do
-    one = benchit(URLEscape).first
-    two = benchit(CGI).first
+    one = benchit(URLEscape, :escape).first
+    two = benchit(CGI, :escape).first
     one.real.should < 20.0 * two.real
   end
 
   it "runs faster than Rack::Utils.escape" do
-    one = benchit(URLEscape).first
-    two = benchit(Rack::Utils).first
+    one = benchit(URLEscape, :escape).first
+    two = benchit(Rack::Utils, :escape).first
     one.real.should < 20.0 * two.real
   end
+
 end
