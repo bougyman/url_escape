2c45d0aba473ff88a9fa9dd478777d24d3045f40
Kevin Berry <KevinBerry@nrs.us>
2009-06-24 12:31:28 -0500

    modified the RBConfig search to take into account suffixed ruby binaries

--- spec/helper.rb
+++ spec/helper.rb
@@ -2,7 +2,7 @@ require 'rbconfig'
 
 unless RUBY_PLATFORM.match(/java/)
   ext = File.expand_path("../../ext", __FILE__)
-  exec_format = RbConfig::CONFIG['ruby_install_name'].sub('ruby', '%s') rescue '%s'
+  exec_format = RbConfig::CONFIG['ruby_install_name'].sub(/^ruby.*$/, '%s') rescue '%s'
 
   system(exec_format % 'rake', 'build')
 
